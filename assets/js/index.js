d379eac2cc7c380f4a91e87f8adcd64fef79616e
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-12-14 13:08:05 +0800

    Project 2 Link update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,67 +1,89 @@
-$(".password-generator").on('click', function() {
-    $( "#dialog-confirm" ).dialog({
-      resizable: false,
-      height: "auto",
-      width: 400,
-      modal: true,
-      buttons: {
-        "Deployed": function() {
-            window.location.href = ("https://ma-fs.github.io/Strong_Password_Generator/");
-        },
-        "Repo": function() {
-            window.location.href = ("https://github.com/MA-FS/Strong_Password_Generator");
-        }
-      }
-    });
-  }) ;
+$(".password-generator").on("click", function () {
+  $("#dialog-confirm").dialog({
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      Deployed: function () {
+        window.location.href =
+          "https://ma-fs.github.io/Strong_Password_Generator/";
+      },
+      Repo: function () {
+        window.location.href =
+          "https://github.com/MA-FS/Strong_Password_Generator";
+      },
+    },
+  });
+});
 
-  $(".weather-dashboard").on('click', function() {
-    $( "#dialog-confirm" ).dialog({
-      resizable: false,
-      height: "auto",
-      width: 400,
-      modal: true,
-      buttons: {
-        "Deployed": function() {
-            window.location.href = ("https://ma-fs.github.io/Weather_Dashboard/");
-        },
-        "Repo": function() {
-            window.location.href = ("https://github.com/MA-FS/Weather_Dashboard");
-        }
-      }
-    });
-  }) ;
+$(".weather-dashboard").on("click", function () {
+  $("#dialog-confirm").dialog({
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      Deployed: function () {
+        window.location.href = "https://ma-fs.github.io/Weather_Dashboard/";
+      },
+      Repo: function () {
+        window.location.href = "https://github.com/MA-FS/Weather_Dashboard";
+      },
+    },
+  });
+});
 
-  $(".coding-quiz").on('click', function() {
-    $( "#dialog-confirm" ).dialog({
-      resizable: false,
-      height: "auto",
-      width: 400,
-      modal: true,
-      buttons: {
-        "Deployed": function() {
-            window.location.href = ("https://ma-fs.github.io/Coding_Quiz/");
-        },
-        "Repo": function() {
-            window.location.href = ("https://github.com/MA-FS/Coding_Quiz");
-        }
-      }
-    });
-  }) ;
+$(".coding-quiz").on("click", function () {
+  $("#dialog-confirm").dialog({
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      Deployed: function () {
+        window.location.href = "https://ma-fs.github.io/Coding_Quiz/";
+      },
+      Repo: function () {
+        window.location.href = "https://github.com/MA-FS/Coding_Quiz";
+      },
+    },
+  });
+});
 
-  $(".crypto-tracker").on('click', function() {
-    $( "#dialog-confirm" ).dialog({
-      resizable: false,
-      height: "auto",
-      width: 400,
-      modal: true,
-      buttons: {
-        "Deployed": function() {
-            window.location.href = ("https://nicoletr.github.io/project1-finance-tracker/");
-        },
-        "Repo": function() {
-            window.location.href = ("https://github.com/nicoletr/project1-finance-tracker");
-        }
-      }
-    });
-  }) ;
+$(".crypto-tracker").on("click", function () {
+  $("#dialog-confirm").dialog({
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      Deployed: function () {
+        window.location.href =
+          "https://nicoletr.github.io/project1-finance-tracker/";
+      },
+      Repo: function () {
+        window.location.href =
+          "https://github.com/nicoletr/project1-finance-tracker";
+      },
+    },
+  });
+});
+
+$(".password-manager").on("click", function () {
+  $("#dialog-confirm").dialog({
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      Deployed: function () {
+        window.location.href = "https://safe-headland-04957.herokuapp.com/";
+      },
+      Repo: function () {
+        window.location.href =
+          "https://github.com/nicoletr/project2-password-manager";
+      },
+    },
+  });
+});
